3a7b4b0b733acdc92bbe4464667f027e712a5433
nit <nit@DESKTOP-UTTU2R1>
2024-12-24 19:22:55 +0530

    sf

---
+++ SB_RS-Actuators_App_02/src/main/java/com/nit/SbRsActuatorsApp02Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRsActuatorsApp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRsActuatorsApp02Application.class, args);
+	}
+
+}
